a0bbacdb945a8060916cbb2b84560b332dbf9d9e
Son Roy Almerol <github@snry.me>
2025-07-23 18:48:04 -0400

    force system wakelock on windows when a backup is ongoing

---
+++ internal/agent/wakelock_windows.go
@@ -0,0 +1,32 @@
+//go:build windows
+
+package agent
+
+import (
+	"syscall"
+)
+
+const (
+	es_continuous       = 0x80000000
+	es_system_required  = 0x00000001
+	es_display_required = 0x00000002
+)
+
+var (
+	kernel32                *syscall.DLL
+	setThreadExecutionState *syscall.Proc
+)
+
+func init() {
+	kernel32, _ = syscall.LoadDLL("kernel32.dll")
+	defer kernel32.Release()
+	setThreadExecutionState = kernel32.MustFindProc("SetThreadExecutionState")
+}
+
+func enableWakeLockSystem() {
+	setThreadExecutionState.Call(es_continuous | es_system_required)
+}
+
+func disableWakeLock() {
+	setThreadExecutionState.Call(es_continuous)
+}
