d750638a4ff7b8925a3cddc53c6506787139ebdc
Son Roy Almerol <github@snry.me>
2025-08-09 02:10:44 -0400

    revert back to minio api

--- go.mod
+++ go.mod
@@ -5,11 +5,6 @@ go 1.24.0
 require (
 	github.com/KimMachineGun/automemlimit v0.7.2
 	github.com/Microsoft/go-winio v0.6.2
-	github.com/aws/aws-sdk-go-v2 v1.37.2
-	github.com/aws/aws-sdk-go-v2/config v1.30.3
-	github.com/aws/aws-sdk-go-v2/credentials v1.18.3
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11
-	github.com/aws/smithy-go v1.22.5
 	github.com/billgraziano/dpapi v0.5.0
 	github.com/containers/winquit v1.1.0
 	github.com/fsnotify/fsnotify v1.9.0
@@ -20,54 +15,51 @@ require (
 	github.com/hanwen/go-fuse/v2 v2.7.2
 	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/kardianos/service v1.2.2
+	github.com/minio/minio-go/v7 v7.0.92
 	github.com/mxk/go-vss v1.2.0
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58
 	github.com/pkg/errors v0.9.1
 	github.com/puzpuzpuz/xsync/v3 v3.5.1
+	github.com/rclone/rclone v1.70.3
 	github.com/rs/zerolog v1.34.0
 	github.com/spf13/cobra v1.9.1
 	github.com/stretchr/testify v1.10.0
 	github.com/xtaci/smux v1.5.34
 	golang.org/x/crypto v0.39.0
-	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394
+	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476
 	golang.org/x/sys v0.33.0
-	golang.org/x/time v0.11.0
+	golang.org/x/time v0.12.0
 	modernc.org/sqlite v1.37.0
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.8 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.14 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.18 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.17 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.27.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.32.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.36.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/jzelinskie/whirlpool v0.0.0-20201016144138-0675e54bb004 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/minio/crc64nvme v1.0.2 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/rs/xid v1.6.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/tinylib/msgp v1.3.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/mod v0.25.0 // indirect
-	golang.org/x/sync v0.15.0 // indirect
-	golang.org/x/tools v0.33.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/libc v1.62.1 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
--- internal/backend/s3/fuse/err.go
+++ internal/backend/s3/fuse/err.go
@@ -4,15 +4,12 @@ package fuse
 
 import (
 	"errors"
-	"net/http"
 	"strings"
 	"syscall"
 
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
-	"github.com/aws/smithy-go"
-	"github.com/hanwen/go-fuse/v2/fs"
+	gofs "github.com/hanwen/go-fuse/v2/fs"
 	"github.com/pbs-plus/pbs-plus/internal/syslog"
+	"github.com/rclone/rclone/fs"
 )
 
 func s3ErrorToErrno(err error) syscall.Errno {
@@ -22,114 +19,24 @@ func s3ErrorToErrno(err error) syscall.Errno {
 
 	syslog.L.Error(err).Write()
 
-	// Handle AWS SDK v2 specific errors
-	var ae smithy.APIError
-	if errors.As(err, &ae) {
-		switch ae.ErrorCode() {
-		case "NoSuchBucket":
-			return syscall.ENOENT
-		case "NoSuchKey":
-			return syscall.ENOENT
-		case "AccessDenied":
-			return syscall.EACCES
-		case "InvalidBucketName":
-			return syscall.EINVAL
-		case "BucketNotEmpty":
-			return syscall.ENOTEMPTY
-		case "InvalidRequest":
-			return syscall.EINVAL
-		case "RequestTimeout":
-			return syscall.ETIMEDOUT
-		case "SlowDown":
-			return syscall.EAGAIN
-		case "ServiceUnavailable":
-			return syscall.EAGAIN
-		case "InternalError":
-			return syscall.EIO
-		default:
-			return syscall.EIO
-		}
-	}
-
-	// Handle specific S3 service exception types
-	var noSuchBucket *types.NoSuchBucket
-	if errors.As(err, &noSuchBucket) {
+	// Handle rclone specific errors
+	if errors.Is(err, fs.ErrorObjectNotFound) {
 		return syscall.ENOENT
 	}
-
-	var noSuchKey *types.NoSuchKey
-	if errors.As(err, &noSuchKey) {
+	if errors.Is(err, fs.ErrorDirNotFound) {
 		return syscall.ENOENT
 	}
-
-	var bucketAlreadyExists *types.BucketAlreadyExists
-	if errors.As(err, &bucketAlreadyExists) {
-		return syscall.EEXIST
+	if errors.Is(err, fs.ErrorPermissionDenied) {
+		return syscall.EACCES
 	}
-
-	var bucketAlreadyOwnedByYou *types.BucketAlreadyOwnedByYou
-	if errors.As(err, &bucketAlreadyOwnedByYou) {
-		return syscall.EEXIST
+	if errors.Is(err, fs.ErrorNotAFile) {
+		return syscall.EISDIR
 	}
-
-	var invalidObjectState *types.InvalidObjectState
-	if errors.As(err, &invalidObjectState) {
+	if errors.Is(err, fs.ErrorIsFile) {
+		return syscall.ENOTDIR
+	}
+	if errors.Is(err, fs.ErrorCantUploadEmptyFiles) {
 		return syscall.EINVAL
-	}
-
-	var objectAlreadyInActiveTierError *types.ObjectAlreadyInActiveTierError
-	if errors.As(err, &objectAlreadyInActiveTierError) {
-		return syscall.EEXIST
-	}
-
-	var objectNotInActiveTierError *types.ObjectNotInActiveTierError
-	if errors.As(err, &objectNotInActiveTierError) {
-		return syscall.ENOENT
-	}
-
-	// Handle HTTP response errors - CORRECTED
-	var responseError *awshttp.ResponseError
-	if errors.As(err, &responseError) {
-		switch responseError.HTTPStatusCode() {
-		case http.StatusNotFound:
-			return syscall.ENOENT
-		case http.StatusForbidden:
-			return syscall.EACCES
-		case http.StatusUnauthorized:
-			return syscall.EACCES
-		case http.StatusBadRequest:
-			return syscall.EINVAL
-		case http.StatusRequestTimeout:
-			return syscall.ETIMEDOUT
-		case http.StatusTooManyRequests:
-			return syscall.EAGAIN
-		case http.StatusInternalServerError:
-			return syscall.EIO
-		case http.StatusBadGateway:
-			return syscall.ECONNREFUSED
-		case http.StatusServiceUnavailable:
-			return syscall.EAGAIN
-		case http.StatusGatewayTimeout:
-			return syscall.ETIMEDOUT
-		default:
-			return syscall.EIO
-		}
-	}
-
-	// Handle operation errors (network issues, etc.)
-	var opError *smithy.OperationError
-	if errors.As(err, &opError) {
-		// Check if it's a network/connection error
-		if strings.Contains(opError.Error(), "connection") {
-			return syscall.ECONNREFUSED
-		}
-		if strings.Contains(opError.Error(), "timeout") {
-			return syscall.ETIMEDOUT
-		}
-		if strings.Contains(opError.Error(), "canceled") {
-			return syscall.ECANCELED
-		}
-		return syscall.EIO
 	}
 
 	// Handle common error strings as fallback
@@ -155,7 +62,7 @@ func s3ErrorToErrno(err error) syscall.Errno {
 		return syscall.EAGAIN
 	default:
 		// Try the default conversion first
-		if errno := fs.ToErrno(err); errno != syscall.EIO {
+		if errno := gofs.ToErrno(err); errno != syscall.EIO {
 			return errno
 		}
 		return syscall.EIO
