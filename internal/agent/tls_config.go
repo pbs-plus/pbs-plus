87c12ce583586547decdb7be6fae037f120b8769
Son Roy Almerol <github@snry.me>
2025-08-15 13:07:50 -0400

    explicitly prioritize aes-gcm for hw accel on tls configs

--- internal/agent/tls_config.go
+++ internal/agent/tls_config.go
@@ -50,6 +50,13 @@ func GetTLSConfig() (*tls.Config, error) {
 	return &tls.Config{
 		Certificates: []tls.Certificate{cert},
 		RootCAs:      rootCAs,
+		MinVersion:   tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		},
 	}, nil
 }
 
