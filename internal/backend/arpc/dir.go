e1339b73bfbaa33968337904cbe5ed211c19f346
Son Roy Almerol <github@snry.me>
2025-08-19 18:57:56 -0400

    optimize memcache to delete entries on time

--- internal/backend/arpc/dir.go
+++ internal/backend/arpc/dir.go
@@ -141,8 +141,8 @@ func (s *DirStream) Next() (fuse.DirEntry, syscall.Errno) {
 		modeBits = fuse.S_IFREG
 	}
 
+	fullPath := filepath.Join(s.path, curr.Name)
 	if !curr.ModTime.IsZero() {
-		fullPath := filepath.Join(s.path, curr.Name)
 		currAttr := types.AgentFileInfo{
 			Name:    curr.Name,
 			Size:    curr.Size,
@@ -155,7 +155,9 @@ func (s *DirStream) Next() (fuse.DirEntry, syscall.Errno) {
 		if err == nil {
 			_ = s.fs.memcache.Set(&memcache.Item{Key: "attr:" + fullPath, Value: attrBytes, Expiration: 0})
 		}
+	}
 
+	if curr.FileAttributes != nil {
 		currXAttr := types.AgentFileInfo{
 			CreationTime:   curr.CreationTime,
 			LastAccessTime: curr.LastAccessTime,
