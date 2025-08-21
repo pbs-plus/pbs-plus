4c38710e4c19e745f3b7eae1f616b83b357d4de2
Son Roy Almerol <github@snry.me>
2025-08-21 12:30:59 -0400

    add restore menu

--- internal/proxy/views/custom/1_navigation.js
+++ internal/proxy/views/custom/1_navigation.js
@@ -17,14 +17,14 @@ Ext.onReady(function() {
         expanded: true,
         children: [],
       });
-      //root.insertChild(index + 1, {
-      //  text: "Disk Restore",
-      //  iconCls: "fa fa-hdd-o",
-      //  id: "backup_restore",
-      //  path: "pbsD2DRestore",
-      //  expanded: true,
-      //  children: [],
-      //});
+      root.insertChild(index + 1, {
+        text: "Disk Restore",
+        iconCls: "fa fa-hdd-o",
+        id: "backup_restore",
+        path: "pbsD2DRestore",
+        expanded: true,
+        children: [],
+      });
     }
   }
 });
