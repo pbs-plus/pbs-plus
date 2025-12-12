808e1010c0b31ae370c9e6f5d26ba829575a6135
Son Roy Almerol <sralmerol@sgl.com>
2025-12-12 11:53:13 -0500

    add http/1.1

--- internal/mtls/mtls.go
+++ internal/mtls/mtls.go
@@ -181,7 +181,7 @@ func BuildServerTLS(serverCertFile, serverKeyFile, caFile, prevCaFile string, cl
 			return &tls.Config{
 				MinVersion:               tls.VersionTLS13,
 				Certificates:             []tls.Certificate{*currentCerts},
-				NextProtos:               []string{"pbsarpc"},
+				NextProtos:               []string{"http/1.1", "pbsarpc"},
 				ClientCAs:                currentCAs,
 				ClientAuth:               clientAuth,
 				PreferServerCipherSuites: true,
@@ -213,7 +213,7 @@ func BuildClientTLS(clientCertPEM, clientKeyPEM, caPEM []byte, legacyCaPEM []byt
 	return &tls.Config{
 		MinVersion:   tls.VersionTLS13,
 		Certificates: []tls.Certificate{cert},
-		NextProtos:   []string{"pbsarpc"},
+		NextProtos:   []string{"http/1.1", "pbsarpc"},
 		RootCAs:      rootCAs,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
