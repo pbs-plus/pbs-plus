9710790ef2b7b8af20bb201838d15a13c1d07a46
Son Roy Almerol <github@snry.me>
2025-12-17 20:44:39 -0500

    revert defaults smux but v1

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -1,10 +1,15 @@
 package utils
 
 import (
+	"log"
+
 	"github.com/pbnjay/memory"
 )
 
+const MaxStreamBuffer = 4 * 1024 * 1024
+
 var MaxConcurrentClients = 16
+var MaxReceiveBuffer = MaxStreamBuffer * MaxConcurrentClients
 
 var IsServer = false
 
@@ -14,12 +19,27 @@ func init() {
 		return
 	}
 
+	ratio := 16
+	for MaxReceiveBuffer < MaxStreamBuffer {
+		if ratio <= 2 {
+			MaxReceiveBuffer = MaxStreamBuffer * 2
+			break
+		}
+
+		MaxReceiveBuffer = int(sysMem.Available) / ratio
+		ratio /= 2
+	}
+
 	gibs := int(sysMem.Total / (1024 * 1024 * 1024))
 	MaxConcurrentClients = gibs
 	if MaxConcurrentClients < 16 {
 		MaxConcurrentClients = 16
 	} else if MaxConcurrentClients > 512 {
 		MaxConcurrentClients = 512
+	}
+
+	if IsServer {
+		log.Printf("initialized aRPC buffer configurations with MaxReceiveBuffer: %d, MaxStreamBuffer: %d, MaxConcurrentClients: %d", MaxReceiveBuffer, MaxStreamBuffer, MaxConcurrentClients)
 	}
 }
 
