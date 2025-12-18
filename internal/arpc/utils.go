618040d994a105505b94f5563bc1abf18f684df7
Son Roy Almerol <github@snry.me>
2025-12-17 21:01:15 -0500

    use smux version 2

--- internal/arpc/utils.go
+++ internal/arpc/utils.go
@@ -35,7 +35,7 @@ func writeErrorResponse(stream io.Writer, status int, err error) {
 
 func defaultConfig() *smux.Config {
 	defaults := smux.DefaultConfig()
-	defaults.Version = 1
+	defaults.Version = 2
 	defaults.MaxReceiveBuffer = utils.MaxReceiveBuffer
 	defaults.MaxStreamBuffer = utils.MaxStreamBuffer
 	defaults.MaxFrameSize = 65535
