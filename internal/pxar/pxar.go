24bb8cd6f4f1edee4725e6e863a0a1c3ab252c24
Son Roy Almerol <sralmerol@sgl.com>
2025-12-18 09:28:29 -0500

    add interface with arpc as bridge for agents

--- internal/pxar/pxar.go
+++ internal/pxar/pxar.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"os"
 	"os/exec"
 	"strings"
 	"sync"
@@ -17,32 +16,6 @@ import (
 	"github.com/fxamacker/cbor/v2"
 	"github.com/pbs-plus/pbs-plus/internal/syslog"
 )
-
-type FileType int
-
-const (
-	FileTypeFile FileType = iota
-	FileTypeDirectory
-	FileTypeSymlink
-	FileTypeHardlink
-	FileTypeDevice
-	FileTypeFifo
-	FileTypeSocket
-)
-
-type EntryInfo struct {
-	FileName        []byte     `cbor:"file_name"`
-	FileType        FileType   `cbor:"file_type"`
-	EntryRangeStart uint64     `cbor:"entry_range_start"`
-	EntryRangeEnd   uint64     `cbor:"entry_range_end"`
-	ContentRange    *[2]uint64 `cbor:"content_range"`
-	Mode            uint64     `cbor:"mode"`
-	UID             uint32     `cbor:"uid"`
-	GID             uint32     `cbor:"gid"`
-	Size            uint64     `cbor:"size"`
-	MtimeSecs       int64      `cbor:"mtime_secs"`
-	MtimeNsecs      uint32     `cbor:"mtime_nsecs"`
-}
 
 type Request struct {
 	_msgpack struct{} `cbor:",toarray"`
@@ -422,94 +395,4 @@ func (c *PxarReader) ListXAttrs(entryStart, entryEnd uint64) (map[string][]byte,
 	}
 
 	return result, nil
-}
-
-func extractEntryInfo(c *PxarReader, resp Response) (*EntryInfo, error) {
-	entryData, ok := resp["Entry"]
-	if !ok {
-		return nil, fmt.Errorf("unexpected response type")
-	}
-
-	entryMap, ok := entryData.(map[any]any)
-	if !ok {
-		return nil, fmt.Errorf("invalid entry data")
-	}
-
-	infoData, ok := entryMap["info"]
-	if !ok {
-		return nil, fmt.Errorf("missing info field")
-	}
-
-	infoBytes, err := c.enc.Marshal(infoData)
-	if err != nil {
-		return nil, fmt.Errorf("failed to re-encode info: %w", err)
-	}
-
-	var info EntryInfo
-	if err := c.dec.Unmarshal(infoBytes, &info); err != nil {
-		return nil, fmt.Errorf("failed to decode info: %w", err)
-	}
-
-	return &info, nil
-}
-
-func (e *EntryInfo) IsDir() bool {
-	return e.FileType == FileTypeDirectory
-}
-
-func (e *EntryInfo) IsFile() bool {
-	return e.FileType == FileTypeFile
-}
-
-func (e *EntryInfo) IsSymlink() bool {
-	return e.FileType == FileTypeSymlink
-}
-
-func (e *EntryInfo) Name() string {
-	return string(e.FileName)
-}
-
-func (e *EntryInfo) ToFileInfo() os.FileInfo {
-	return &fileInfo{entry: e}
-}
-
-type fileInfo struct {
-	entry *EntryInfo
-}
-
-func (fi *fileInfo) Name() string {
-	return fi.entry.Name()
-}
-
-func (fi *fileInfo) Size() int64 {
-	return int64(fi.entry.Size)
-}
-
-func (fi *fileInfo) Mode() os.FileMode {
-	mode := os.FileMode(fi.entry.Mode & 0777)
-	switch fi.entry.FileType {
-	case FileTypeDirectory:
-		mode |= os.ModeDir
-	case FileTypeSymlink:
-		mode |= os.ModeSymlink
-	case FileTypeDevice:
-		mode |= os.ModeDevice
-	case FileTypeFifo:
-		mode |= os.ModeNamedPipe
-	case FileTypeSocket:
-		mode |= os.ModeSocket
-	}
-	return mode
-}
-
-func (fi *fileInfo) ModTime() time.Time {
-	return time.Unix(0, fi.entry.MtimeSecs)
-}
-
-func (fi *fileInfo) IsDir() bool {
-	return fi.entry.IsDir()
-}
-
-func (fi *fileInfo) Sys() any {
-	return fi.entry
 }
---
+++ internal/pxar/restore.go
@@ -0,0 +1,88 @@
+package pxar
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+)
+
+func RestoreToLocal(ctx context.Context, rootDir string, client *RemoteClient) error {
+	if err := os.MkdirAll(rootDir, 0o755); err != nil {
+		return fmt.Errorf("mkdir root: %w", err)
+	}
+	root, err := client.GetRoot(ctx)
+	if err != nil {
+		return err
+	}
+	if !root.IsDir() {
+		return fmt.Errorf("archive root is not a directory")
+	}
+	return restoreDir(ctx, client, rootDir, root)
+}
+
+func restoreFile(ctx context.Context, client *RemoteClient, path string, e EntryInfo) error {
+	mode := os.FileMode(e.Mode & 0777)
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, mode)
+	if err != nil {
+		return fmt.Errorf("create file %q: %w", path, err)
+	}
+	defer f.Close()
+
+	if e.ContentRange != nil {
+		start, end := e.ContentRange[0], e.ContentRange[1]
+		var off uint64
+		buf := make([]byte, 1<<20)
+
+		for off < e.Size {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+			}
+
+			size := uint(len(buf))
+			remain := e.Size - off
+			if remain < uint64(size) {
+				size = uint(remain)
+			}
+			data, err := client.Read(ctx, start, end, off, size)
+			if err != nil {
+				return fmt.Errorf("read content %q: %w", path, err)
+			}
+			if len(data) == 0 {
+				break
+			}
+			if _, err := f.Write(data); err != nil {
+				return fmt.Errorf("write content %q: %w", path, err)
+			}
+			off += uint64(len(data))
+		}
+	}
+
+	return applyMeta(path, e)
+}
+
+func restoreSymlink(ctx context.Context, client *RemoteClient, path string, e EntryInfo) error {
+	target, err := client.ReadLink(ctx, e.EntryRangeStart, e.EntryRangeEnd)
+	if err != nil {
+		return fmt.Errorf("readlink data %q: %w", path, err)
+	}
+	if err := os.Symlink(string(target), path); err != nil {
+		return fmt.Errorf("symlink %q: %w", path, err)
+	}
+	return applyMetaSymlink(path, e)
+}
+
+func applyMeta(path string, e EntryInfo) error {
+	_ = os.Chmod(path, os.FileMode(e.Mode&0777))
+	_ = os.Chown(path, int(e.UID), int(e.GID))
+	mt := time.Unix(e.MtimeSecs, int64(e.MtimeNsecs))
+	_ = os.Chtimes(path, mt, mt)
+	return nil
+}
+
+func applyMetaSymlink(path string, e EntryInfo) error {
+	_ = os.Lchown(path, int(e.UID), int(e.GID))
+	return nil
+}
