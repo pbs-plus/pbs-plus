b4f21c49910c3ed5317a00f22a4127b73e0c2b3e
Son Roy Almerol <github@snry.me>
2025-12-20 18:17:13 -0500

    add local interface ip check

--- internal/utils/is_localhost.go
+++ internal/utils/is_localhost.go
@@ -16,5 +16,50 @@ func IsLocalhost(r *http.Request) bool {
 		return false
 	}
 
-	return ip.IsLoopback()
+	if ip.IsLoopback() {
+		return true
+	}
+
+	if isLocalInterfaceIP(ip) {
+		return true
+	}
+
+	return false
+}
+
+func isLocalInterfaceIP(ip net.IP) bool {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return false
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+		for _, a := range addrs {
+			var ipNet *net.IPNet
+			switch v := a.(type) {
+			case *net.IPNet:
+				ipNet = v
+			case *net.IPAddr:
+				ipNet = &net.IPNet{IP: v.IP, Mask: net.CIDRMask(128, 128)} // exact match
+			default:
+				continue
+			}
+			if ipNet == nil || ipNet.IP == nil {
+				continue
+			}
+			if (ip.To4() != nil && ipNet.IP.To4() == nil) || (ip.To4() == nil && ipNet.IP.To4() != nil) {
+				continue
+			}
+			if ipNet.Contains(ip) {
+				return true
+			}
+		}
+	}
+	return false
 }
