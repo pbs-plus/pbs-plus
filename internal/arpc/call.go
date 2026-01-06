777c0dad5e36df0ec8c02f7d4b2a075b2a7bdece
Son Roy Almerol <github@snry.me>
2026-01-06 08:05:09 -0500

    fix stream leak

--- internal/arpc/call.go
+++ internal/arpc/call.go
@@ -173,6 +173,7 @@ func (s *StreamPipe) CallBinary(ctx context.Context, method string, payload any,
 	if err != nil {
 		return 0, err
 	}
+	defer stream.Close()
 
 	if resp.Status != 213 {
 		var serErr SerializableError
