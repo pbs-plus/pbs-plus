7c7b060159884a1861ab12c00a3a5471cf9a4b7f
Son Roy Almerol <sralmerol@sgl.com>
2026-01-12 14:57:01 -0500

    adjust pxar api for compatibility

--- internal/pxar/entry_info.go
+++ internal/pxar/entry_info.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-type FileType int
+type FileType uint8
 
 const (
 	FileTypeFile FileType = iota
@@ -18,49 +18,36 @@ const (
 )
 
 type EntryInfo struct {
-	FileName        []byte     `cbor:"file_name"`
-	FileType        FileType   `cbor:"file_type"`
-	EntryRangeStart uint64     `cbor:"entry_range_start"`
-	EntryRangeEnd   uint64     `cbor:"entry_range_end"`
-	ContentRange    *[2]uint64 `cbor:"content_range"`
-	Mode            uint64     `cbor:"mode"`
-	UID             uint32     `cbor:"uid"`
-	GID             uint32     `cbor:"gid"`
-	Size            uint64     `cbor:"size"`
-	MtimeSecs       int64      `cbor:"mtime_secs"`
-	MtimeNsecs      uint32     `cbor:"mtime_nsecs"`
+	FileName        []byte   `cbor:"file_name"`
+	FileType        FileType `cbor:"file_type"`
+	EntryRangeStart uint64   `cbor:"entry_range_start"`
+	EntryRangeEnd   uint64   `cbor:"entry_range_end"`
+	// Option<(u64, u64)> in Rust becomes a 2-element array or nil in CBOR
+	ContentRange []uint64 `cbor:"content_range"`
+	Mode         uint64   `cbor:"mode"`
+	UID          uint32   `cbor:"uid"`
+	GID          uint32   `cbor:"gid"`
+	Size         uint64   `cbor:"size"`
+	MtimeSecs    int64    `cbor:"mtime_secs"`
+	MtimeNsecs   uint32   `cbor:"mtime_nsecs"`
 }
 
-func (e *EntryInfo) IsDir() bool {
-	return e.FileType == FileTypeDirectory
-}
-
-func (e *EntryInfo) IsFile() bool {
-	return e.FileType == FileTypeFile
-}
-
-func (e *EntryInfo) IsSymlink() bool {
-	return e.FileType == FileTypeSymlink
-}
-
-func (e *EntryInfo) Name() string {
-	return string(e.FileName)
-}
+func (e *EntryInfo) IsDir() bool  { return e.FileType == FileTypeDirectory }
+func (e *EntryInfo) Name() string { return string(e.FileName) }
 
 func (e *EntryInfo) ToFileInfo() os.FileInfo {
 	return &fileInfo{entry: e}
 }
 
-type fileInfo struct {
-	entry *EntryInfo
-}
+type fileInfo struct{ entry *EntryInfo }
 
-func (fi *fileInfo) Name() string {
-	return fi.entry.Name()
-}
+func (fi *fileInfo) Name() string { return fi.entry.Name() }
+func (fi *fileInfo) Size() int64  { return int64(fi.entry.Size) }
+func (fi *fileInfo) IsDir() bool  { return fi.entry.IsDir() }
+func (fi *fileInfo) Sys() any     { return fi.entry }
 
-func (fi *fileInfo) Size() int64 {
-	return int64(fi.entry.Size)
+func (fi *fileInfo) ModTime() time.Time {
+	return time.Unix(fi.entry.MtimeSecs, int64(fi.entry.MtimeNsecs))
 }
 
 func (fi *fileInfo) Mode() os.FileMode {
@@ -78,16 +65,4 @@ func (fi *fileInfo) Mode() os.FileMode {
 		mode |= os.ModeSocket
 	}
 	return mode
-}
-
-func (fi *fileInfo) ModTime() time.Time {
-	return time.Unix(0, fi.entry.MtimeSecs)
-}
-
-func (fi *fileInfo) IsDir() bool {
-	return fi.entry.IsDir()
-}
-
-func (fi *fileInfo) Sys() any {
-	return fi.entry
 }
