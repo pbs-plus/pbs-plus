1cad68a47434a3343f64df18d692881b3617b7c7
Son Roy Almerol <sralmerol@sgl.com>
2026-01-12 14:35:19 -0500

    add path selector for snapshot

---
+++ internal/web/views/custom/windows/path_selector.js
@@ -0,0 +1,42 @@
+Ext.define("PBS.window.D2DPathSelector", {
+  extend: "Proxmox.window.FileBrowser",
+  alias: "widget.pbsD2DPathSelector",
+
+  title: gettext("Select Path"),
+
+  controller: {
+    xclass: "Ext.app.ViewController",
+    init: function (view) {
+      this.constructor.prototype.init.apply(this, arguments);
+    },
+
+    onSelect: function () {
+      let me = this;
+      let tree = me.lookup("tree");
+      let selection = tree.getSelection();
+      if (selection && selection.length > 0) {
+        let data = selection[0].data;
+        let path = atob(data.filepath);
+        me.getView().fireEvent("select", path);
+        me.getView().close();
+      }
+    },
+
+    fileChanged: function () {
+      let me = this;
+      let tree = me.lookup("tree");
+      let selection = tree.getSelection();
+      me.lookup("selectBtn").setDisabled(!selection || selection.length < 1);
+    },
+  },
+
+  fbar: [
+    {
+      xtype: "button",
+      text: gettext("Select"),
+      reference: "selectBtn",
+      disabled: true,
+      handler: "onSelect",
+    },
+  ],
+});
