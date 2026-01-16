e6b358c51fe509d7a68eac9c18f269f1223ad5de
Son Roy Almerol <sralmerol@sgl.com>
2026-01-16 16:12:37 -0500

    update to VAL-13.0.1 branch

--- internal/agent/snapshots/blksnap/control.go
+++ internal/agent/snapshots/blksnap/control.go
@@ -38,27 +38,46 @@ func (c *Control) GetVersion() (*Version, error) {
 	return &version, nil
 }
 
+func (c *Control) GetMod() (*Mod, error) {
+	var m Mod
+	if err := ioctl(c.fd.Fd(), IOCTL_BLKSNAP_MOD, uintptr(unsafe.Pointer(&m))); err != nil {
+		return nil, err
+	}
+	return &m, nil
+}
+
+func (c *Control) SetLog(path string, level int32, tzMinutes int32) error {
+	p, err := unix.BytePtrFromString(path)
+	if err != nil {
+		return err
+	}
+	req := SetLog{
+		TzMinuteswest: tzMinutes,
+		Level:         level,
+		FilepathSize:  uint32(len(path)),
+		Filepath:      uint64(uintptr(unsafe.Pointer(p))),
+	}
+	return ioctl(c.fd.Fd(), IOCTL_BLKSNAP_SETLOG, uintptr(unsafe.Pointer(&req)))
+}
+
 type Snapshot struct {
 	ID      UUID
 	control *Control
 }
 
 func (c *Control) CreateSnapshot(filename string, limitSectors uint64) (*Snapshot, error) {
-	filenameBytes := append([]byte(filename), 0) // null-terminated
-
+	p, err := unix.BytePtrFromString(filename)
+	if err != nil {
+		return nil, err
+	}
 	req := snapshotCreate{
 		DiffStorageLimitSect: limitSectors,
-		DiffStorageFilename:  uint64(uintptr(unsafe.Pointer(&filenameBytes[0]))),
+		DiffStorageFilename:  uint64(uintptr(unsafe.Pointer(p))),
 	}
-
 	if err := ioctl(c.fd.Fd(), IOCTL_BLKSNAP_SNAPSHOT_CREATE, uintptr(unsafe.Pointer(&req))); err != nil {
-		return nil, fmt.Errorf("failed to create snapshot: %w", err)
+		return nil, err
 	}
-
-	return &Snapshot{
-		ID:      req.ID,
-		control: c,
-	}, nil
+	return &Snapshot{ID: req.ID, control: c}, nil
 }
 
 func (c *Control) OpenSnapshot(id UUID) *Snapshot {
@@ -80,6 +99,20 @@ func (s *Snapshot) Destroy() error {
 		return fmt.Errorf("failed to destroy snapshot: %w", err)
 	}
 	return nil
+}
+
+func (s *Snapshot) AppendStorage(devPath string, ranges []Range) error {
+	p, err := unix.BytePtrFromString(devPath)
+	if err != nil {
+		return err
+	}
+	req := snapshotAppendStorage{
+		ID:      s.ID,
+		Devpath: uint64(uintptr(unsafe.Pointer(p))),
+		Count:   uint32(len(ranges)),
+		Ranges:  uint64(uintptr(unsafe.Pointer(&ranges[0]))),
+	}
+	return ioctl(s.control.fd.Fd(), IOCTL_BLKSNAP_SNAPSHOT_APPEND_STORAGE, uintptr(unsafe.Pointer(&req)))
 }
 
 func (c *Control) CollectSnapshots(maxCount uint32) ([]UUID, error) {
