b3aa13061f698729975044dd9fb059fd9f1d43a3
Son Roy Almerol <sralmerol@sgl.com>
2026-02-04 09:52:55 -0500

    separate file handle for windows dir reader

--- internal/agent/agentfs/agentfs_windows.go
+++ internal/agent/agentfs/agentfs_windows.go
@@ -51,6 +51,13 @@ func (s *AgentFSServer) platformOpen(path string) (*FileHandle, error) {
 	fh.fileSize = std.EndOfFile
 	fh.isDir = std.Directory != 0
 	if fh.isDir {
+		fh.file.Close()
+		fh.file = nil
+		raw, err := windows.CreateFile(pathUTF16, windows.GENERIC_READ, windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE|windows.FILE_SHARE_DELETE, nil, windows.OPEN_EXISTING, windows.FILE_FLAG_BACKUP_SEMANTICS, 0)
+		if err != nil {
+			return nil, err
+		}
+		f := os.NewFile(uintptr(raw), path)
 		reader, err := NewDirReader(f, path)
 		if err != nil {
 			f.Close()
