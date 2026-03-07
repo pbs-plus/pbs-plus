5514b87ac26d8d3813bfc50c4289c5af2e266367
Son Roy Almerol <github@snry.me>
2026-03-06 21:18:44 -0500

    linux/updater: just exit process and let supervisor handle the restart after binary download

--- internal/agent/updater/restart_unix.go
+++ internal/agent/updater/restart_unix.go
@@ -2,55 +2,6 @@
 
 package updater
 
-import (
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"syscall"
-
-	"github.com/pbs-plus/pbs-plus/internal/syslog"
-)
-
 func restartCallback(_ Config) bool {
-	exePath, err := os.Executable()
-	if err != nil {
-		syslog.L.Error(err).WithMessage("failed to get executable path for restart").Write()
-		return false
-	}
-
-	dir := filepath.Dir(exePath)
-	filename := filepath.Base(exePath)
-
-	if strings.HasPrefix(filename, ".") && strings.HasSuffix(filename, ".old") {
-		newBase := strings.TrimSuffix(filename, ".old")
-		newBase = strings.TrimPrefix(newBase, ".")
-
-		exePath = filepath.Join(dir, newBase)
-	}
-
-	cmd := exec.Command(exePath, "restart")
-
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setsid: true,
-	}
-
-	cmd.Stdout = nil
-	cmd.Stderr = nil
-	cmd.Stdin = nil
-
-	if err := cmd.Start(); err != nil {
-		syslog.L.Error(err).
-			WithMessage("failed to spawn self restart helper").
-			WithField("target", exePath).
-			Write()
-	} else {
-		syslog.L.Info().
-			WithMessage("spawned self restart helper process").
-			WithField("target", exePath).
-			Write()
-		_ = cmd.Process.Release()
-	}
-
 	return false
 }
