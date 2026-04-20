8663fad9b2857c0af718863097b3c91506bf0028
Son Roy Almerol <sralmerol@sgl.com>
2026-04-20 11:25:00 -0400

    refactor: create conf package and promote utils

---
+++ internal/conf/buffer.go
@@ -0,0 +1,55 @@
+package conf
+
+import (
+	"log"
+
+	"github.com/pbnjay/memory"
+)
+
+const MaxStreamBuffer = 4 * 1024 * 1024
+
+var MaxConcurrentClients = 16
+var MaxReceiveBuffer = MaxStreamBuffer * MaxConcurrentClients
+var IsServer = false
+
+func InitBuffers() {
+	sysMem, err := GetSysMem()
+	if err != nil {
+		return
+	}
+
+	ratio := 16
+	for MaxReceiveBuffer < MaxStreamBuffer {
+		if ratio <= 2 {
+			MaxReceiveBuffer = MaxStreamBuffer * 2
+			break
+		}
+
+		MaxReceiveBuffer = int(sysMem.Available) / ratio
+		ratio /= 2
+	}
+
+	gibs := int(sysMem.Total / (1024 * 1024 * 1024))
+	MaxConcurrentClients = gibs
+	if MaxConcurrentClients < 16 {
+		MaxConcurrentClients = 16
+	} else if MaxConcurrentClients > 512 {
+		MaxConcurrentClients = 512
+	}
+
+	if IsServer {
+		log.Printf("initialized aRPC buffer configurations with MaxReceiveBuffer: %d, MaxStreamBuffer: %d, MaxConcurrentClients: %d", MaxReceiveBuffer, MaxStreamBuffer, MaxConcurrentClients)
+	}
+}
+
+type SysMem struct {
+	Total     uint64
+	Available uint64
+}
+
+func GetSysMem() (*SysMem, error) {
+	return &SysMem{
+		Total:     memory.TotalMemory(),
+		Available: memory.FreeMemory(),
+	}, nil
+}
